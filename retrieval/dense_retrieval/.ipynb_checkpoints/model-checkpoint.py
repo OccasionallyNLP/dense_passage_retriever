d6c1c110e39cfd9ecbad295f4457887d8cc6fec9
OccasionallyNLP <tillcry8@gmail.com>
2023-01-25 18:45:04 +0900

    check

---
+++ retrieval/dense_retrieval/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 17:40:42 2021
+
+@author: OK
+"""
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch import tensor as T
+from transformers import PreTrainedModel,RobertaModel, BertModel,T5EncoderModel
+
+class Encoder(PreTrainedModel):
+    def __init__(self, config, args, model):
+        super().__init__(config)
+        self.args = args
+        self.pretrained_model = model(config)
+        
+    def init_pretrained_model(self, state_dict):
+        # init pretrained model
+        self.pretrained_model.load_state_dict(state_dict) 
+    
+    def forward(self, input_ids, attention_mask, token_type_ids, **kwargs):
+        if isinstance(self.pretrained_model,BertModel):
+            output = self.pretrained_model(input_ids, attention_mask, token_type_ids)
+        else: 
+            output = self.pretrained_model(input_ids, attention_mask) # output[0] ~ (bs, seq_len, dim)
+        
+        if self.args.pool=='cls':
+            out = output['pooler_output'] # bs, dim
+        elif self.args.pool == 'mean':
+            out = output['last_hidden_states'].masked_fill(attention_mask.unsqueeze(2).repeat(1,1,self.config.hidden_size)==0,0) # bs, seq_len, dim
+            out = out.sum(dim=1) # bs, dim
+            s = attention_mask.sum(-1, keepdim=True) # bs, 1
+            out = out/(s+1e-12)
+        return out
+    
+class DprEncoder(nn.Module):
+    def __init__(self, encoder_p:PreTrainedModel, encoder_q:PreTrainedModel, fix_p_encoder : bool = False, fix_q_encoder : bool = False):
+        super().__init__()
+        self.passage_encoder = encoder_p
+        self.question_encoder = encoder_q
+        self.fix_p_encoder = fix_p_encoder
+        self.fix_q_encoder = fix_q_encoder
+        
+    def forward(self, passage_input_ids:T, passage_attention_mask:T, passage_token_type_ids:T, question_input_ids:T, question_attention_mask:T, question_token_type_ids:T, labels:None):
+        if self.fix_p_encoder:
+            with torch.no_grad():
+                ep = self.passage_encoder(input_ids = passage_input_ids, attention_mask = passage_attention_mask, token_type_ids =  passage_token_type_ids)
+        else:
+            ep = self.passage_encoder(input_ids = passage_input_ids, attention_mask = passage_attention_mask, token_type_ids = passage_token_type_ids)
+        if self.fix_q_encoder:
+            with torch.no_grad():
+                eq = self.question_encoder(input_ids = question_input_ids, attention_mask = question_attention_mask, token_type_ids = question_token_type_ids)
+        else:
+            eq = self.question_encoder(input_ids = question_input_ids, attention_mask = question_attention_mask, token_type_ids = question_token_type_ids)
+        if labels is None:
+            return ep, eq # (bs, dim), (bs,dim)
+        else:
+            score = eq.matmul(ep.T) # (bs,bs) or (bs, 2bs)
+            loss = F.nll_loss(F.log_softmax(score,dim=-1),labels)
+            return ep, eq, score, loss
+        
